de4fd73474d4c80ff5dc52d16f6734570fb15095
sumanta1998 <sumanta.paikaray1998@gmail.com>
2022-07-19 16:42:59 +0530

    Updating user model

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      // required: true,
+      required: true,
       index: { unique: true },
     },
     email: {
